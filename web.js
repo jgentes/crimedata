09a2690e6a5138c0c5b39a7b19cbda11f9c3f1bf
James Gentes <james@thesocialbusiness.com>
2014-06-16 21:19:03 +0000

    tracking working!

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var bodyParser = require('body-parser')
 var logfmt = require("logfmt");
+var request = require("request");
 var spawn = require('child_process').spawn;
+
 var app = express();
 
 app.use(logfmt.requestLogger());
@@ -39,27 +41,29 @@ function spokanevalley_status(tracking_number, res) {
   
   console.log("Tracking Number: " + tracking_number);
   
-  var child = spawn("casperjs", ["test", "spokanevalley/submit.js", "--actionid=" + actionid, "--desc=" + desc]);
-  
-  child.stdout.on('data', function (data) {
-    data = data.toString();
-    //console.log(data); //REQUIRED FOR DEBUGGING CASPER - uncomment to see casper data log
-    if (data.search("Tracking Number:") === 0) {
-      
-      var tracking_number = data.substr(17, data.length - 17);
-      console.log("Tracking Number: " + tracking_number);
-      res.send(201, 'Tracking Number: ' + tracking_number);
-      
-    } else if (data.search("Error:") === 0) {
-      
-      var response = data.substr(7, data.length - 7);
-      res.send(202, 'Error: ' + response);
-      var error = new Error(response);
-      console.log(error.stack)
-      
-    };
-    
-  });
+  request.post(
+    'http://www.egovlink.com/spokanevalley/action_request_lookup.asp',
+    { form: { REQUEST_ID: tracking_number } },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          
+          var start = body.search('C.A.R.E.S. Activity') + 29;
+          var end = body.search('http://www.egovlink.com/spokanevalley/img/clearshim.gif');
+          
+          if (start === 28) {
+            start = body.search('C.A.R.E.S. Lookup') + 24;
+            end = end - 42;
+          } else {
+            end = end - 84;
+          }
+          
+          var status = body.substr(start, end - start);
+          console.log(status);
+          
+          res.send(200, status);
+        }
+    }
+  );
   
 };
 
@@ -72,8 +76,11 @@ app.post('/new', bodyParser(), function(req, res) {
   var tracking_number = spokanevalley_submit(actionid, desc, res);
 });
 
-app.post('/response', bodyParser(), function(req, res) {
+app.post('/status', bodyParser(), function(req, res) {
   var tracking_number = req.body.tracking_number;
+  if (tracking_number === undefined) {
+    res.send(202, 'Error: tracking_number is required!');
+  }
   var status = spokanevalley_status(tracking_number, res);
 });
 
