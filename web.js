d6ce165b98434976304fb9dd563c87ed80b7c236
James Gentes <james@thesocialbusiness.com>
2014-06-16 18:06:24 +0000

    switched to POST for new and status

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var bodyParser = require('body-parser')
 var logfmt = require("logfmt");
 var spawn = require('child_process').spawn;
 var app = express();
@@ -34,10 +35,50 @@ function spokanevalley_submit(actionid, desc, res) {
   
 };
 
-app.get('/new', function(req, res) {
-  var actionid = req.query.actionid;
-  var desc = req.query.desc;
+function spokanevalley_status(tracking_number, res) {
+  
+  console.log("Tracking Number: " + tracking_number);
+  
+  var child = spawn("casperjs", ["test", "spokanevalley/submit.js", "--actionid=" + actionid, "--desc=" + desc]);
+  
+  child.stdout.on('data', function (data) {
+    data = data.toString();
+    //console.log(data); //REQUIRED FOR DEBUGGING CASPER - uncomment to see casper data log
+    if (data.search("Tracking Number:") === 0) {
+      
+      var tracking_number = data.substr(17, data.length - 17);
+      console.log("Tracking Number: " + tracking_number);
+      res.send(201, 'Tracking Number: ' + tracking_number);
+      
+    } else if (data.search("Error:") === 0) {
+      
+      var response = data.substr(7, data.length - 7);
+      res.send(202, 'Error: ' + response);
+      var error = new Error(response);
+      console.log(error.stack)
+      
+    };
+    
+  });
+  
+};
+
+app.post('/new', bodyParser(), function(req, res) {
+  var actionid = req.body.actionid;
+  var desc = req.body.desc;
+  if (actionid === undefined || desc === undefined) {
+    res.send(202, 'Error: ActionID and Description are required!');
+  }
   var tracking_number = spokanevalley_submit(actionid, desc, res);
+});
+
+app.post('/response', bodyParser(), function(req, res) {
+  var tracking_number = req.body.tracking_number;
+  var status = spokanevalley_status(tracking_number, res);
+});
+
+app.get('/', function(req, res) {
+  res.send(404, 'Oops, nothing here!');
 });
 
 var port = Number(process.env.PORT);
